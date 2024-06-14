6504eca9b53d701addf850b2ec5518c3d2ad7d7b
l3in99 <gkqls99@naver.com>
2024-06-14 15:04:32 +0900

    sd

--- src/main/java/com/example/basic/model/TableExam1.java
+++ src/main/java/com/example/basic/model/TableExam1.java
@@ -2,11 +2,15 @@ package com.example.basic.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
 @Entity(name = "table_exam_1")
 public class TableExam1 {
     @Id
+    @GeneratedValue
     Integer id;
     @Column(length = 100, nullable = false)
     String title;
