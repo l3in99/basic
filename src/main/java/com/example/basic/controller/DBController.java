405e486e7bb2b38a8b2b9b8bb7c829fd7b801d20
l3in99 <gkqls99@naver.com>
2024-06-14 15:34:09 +0900

    ,

--- src/main/java/com/example/basic/controller/DBController.java
+++ src/main/java/com/example/basic/controller/DBController.java
@@ -1,21 +1,69 @@
 package com.example.basic.controller;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.basic.dao.DemoDao;
 import com.example.basic.mapper.DemoMapper;
 import com.example.basic.mapper.EmpMapper;
+import com.example.basic.model.TableExam1;
+import com.example.basic.model.major;
+import com.example.basic.repository.TableExam1Repository;
+import com.example.basic.repository.majorRepository;
 
 @RestController
 public class DBController {
-    @Autowired
-    DemoDao demoDao;
+    @Autowired DemoDao demoDao;
+    @Autowired TableExam1Repository tableExam1Repository;
+
+    @Autowired majorRepository mr;
+    @GetMapping("major/add")
+    public major majoradd(@ModelAttribute major major){
+        major.setEbtbDate(new Date());
+        return mr.save(major);
+    }
+
+    @GetMapping("major/list")
+    public List<major> majorlist(){
+        return mr.findAll();
+    }
+
+    @GetMapping("/table1/add")
+    public String table1Add(@RequestParam String title) {
+        TableExam1 t1 = new TableExam1();
+        t1.setTitle(title);
+        tableExam1Repository.save(t1);
+        return "입력 완료";
+    }
+
+    @GetMapping("/table1/remove")
+    public String table1Remove(@RequestParam int id) {
+        Optional<TableExam1> t = tableExam1Repository.findById(id);
+        tableExam1Repository.delete(t.get());
+        return "삭제 완료";
+    }
+
+    @GetMapping("/table1/list")
+    public List<TableExam1> table1find() {
+        List<TableExam1> list = tableExam1Repository.findAll();
+        return list;
+    }
+
+    @GetMapping("/table1/{id}")
+    public TableExam1 table1find2(@PathVariable int id) {
+        Optional<TableExam1> exam1 = tableExam1Repository.findById(id);
+        return exam1.get();
+    }
 
     @GetMapping("/jdbc/demo")
     public List<Map<String, Object>> jdbcDemo() {
---
+++ src/main/java/com/example/basic/repository/majorRepository.java
@@ -0,0 +1,10 @@
+package com.example.basic.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.basic.model.major;
+
+
+public interface majorRepository extends JpaRepository<major, Integer> {
+
+}
