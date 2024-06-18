b62472380e05cc9e83cd0610325f2edddbc9692b
l3in99 <gkqls99@naver.com>
2024-06-18 11:19:02 +0900

    반복문

--- src/main/java/com/example/basic/controller/ThymeleafController.java
+++ src/main/java/com/example/basic/controller/ThymeleafController.java
@@ -49,4 +49,27 @@ public class ThymeleafController {
         model.addAttribute("user", user);
         return "user";
     }
+
+    @GetMapping("userList")
+    public String userList(Model model) {
+        List<Map<String, Object>> userList = new ArrayList<>();
+        Map<String, Object> user = null;
+        user = new HashMap<>();
+        user.put("userId", "a");
+        user.put("userName", "apple");
+        user.put("userAge", 21);
+        userList.add(user);
+        user = new HashMap<>();
+        user.put("userId", "b");
+        user.put("userName", "banana");
+        user.put("userAge", 22);
+        userList.add(user);
+        user = new HashMap<>();
+        user.put("userId", "c");
+        user.put("userName", "carrot");
+        user.put("userAge", 23);
+        userList.add(user);
+        model.addAttribute("userList", userList);
+        return "userList";
+    }
 }
